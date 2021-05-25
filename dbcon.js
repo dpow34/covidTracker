d8f97a495c6178ddba858f943cea9d734504ac16
powdrild <55857022+powdrild@users.noreply.github.com>
2021-05-24 22:47:51 -0500

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_powdrild',
-  password        : '5835',
-  database        : 'cs340_powdrild'
-});
-module.exports.pool = pool;
